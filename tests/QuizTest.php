85bff2c690d1c131dfcd140834aa6b95541f4939
Jevgenija Potalujeva <jpotalujeva@gmail.com>
2016-12-01 17:31:10 +0200

    Update QuizTest.php

--- tests/QuizTest.php
+++ tests/QuizTest.php
@@ -7,6 +7,6 @@ class QuizTest extends PHPUnit_Framework_TestCase
     public function testAddQuestion()
     {
         $quiz = new Quiz();
-        $this->assertEquals($quiz->addQuestion())
+        $this->assertEquals($quiz->addQuestion());
     }
 }
